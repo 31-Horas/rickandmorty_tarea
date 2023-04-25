cfb1343fc33993bf4a9061d13a882cee51334039
fernandocamacho <salvadorfernando200@outlook.com>
2023-04-24 20:06:00 -0600

    docker rick and morty tarea

--- class/src/keys.js
+++ class/src/keys.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+console.log(process.env)
+
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'mysql',
-        database: 'itesm502'
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
     }
 }
